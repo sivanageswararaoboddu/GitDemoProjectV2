cf58c526c0eae5d69071128d933b3bb284ba9625
sivanage <sivanage@QAFrameWorkVM6.amr.corp.intel.com>
2021-08-24 16:15:36 -0700

    second commit

--- GitDemoProjectV2/src/test/java/firstPkg/FirstPgm.java
+++ GitDemoProjectV2/src/test/java/firstPkg/FirstPgm.java
@@ -5,6 +5,8 @@ public class FirstPgm {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		System.out.println("Second line for second commit");
+		
 	}
 
 }
